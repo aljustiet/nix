9fd5e5e3f861d593cc72c0e3c4fad749edd2fb8a
aljustiet <aljustiet@tutamail.com>
2025-09-19 18:31:35 +0500

    update

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -7,6 +7,7 @@
     zen-browser.enable = true;
     zsh.historySubstringSearch.enable = true;
   };
+  system.autoUpgrade.enable = true;
   xdg.userDirs.enable = true;
   xdg.userDirs.createDirectories = true;
   home.username = "aljustiet";
