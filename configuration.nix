dee8b529444c3da062224e7c78201cc4ff83066f
aljustiet <aljustiet@tutamail.com>
2025-09-19 16:27:13 +0500

    fix typo

--- configuration.nix
+++ configuration.nix
@@ -165,7 +165,7 @@
       enable = true;
       alsa.enable = true;
       pulse.enable = true;
-      wireplumbler.enable = true;
+      wireplumber.enable = true;
       jack.enable = true;
     };
   };
