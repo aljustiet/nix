2622501cb4fce114088aa14142c347537ddc251b
aljustiet <aljustiet@tutamail.com>
2025-10-06 13:34:34 +0500

    update auto-cpufreq

--- configuration.nix
+++ configuration.nix
@@ -192,6 +192,22 @@
     flatpak.enable = true;
     auto-cpufreq = {
       enable = true;
+      settings = {
+        charger = {
+          governor = "performance";
+          energy_performance_preference = "performance";
+          platform_profile = "performance";
+          turbo = "auto";
+        };
+        battery = {
+          governor = "powersave";
+          energy_performance_preference = "power";
+          platform_profile = "low-power";
+          turbo = "never";
+          enable_thresholds = true;
+          start_threshold = 20;
+          stop_threshold = 80;
+      };
     };
     pipewire = {
       enable = true;
