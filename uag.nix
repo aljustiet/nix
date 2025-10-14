268ca746b4d1fe9174c139319c012492b3e9cf3f
aljustiet <aljustiet@tutamail.com>
2025-10-14 20:20:16 +0500

    update

--- uag.nix
+++ uag.nix
@@ -4,10 +4,27 @@
   lib,
   pkgs,
   ...
-}: {
-  services.getty.autologinUser = "aljustiet";
-  users.users.root = {
-    shell = pkgs.fish;
+}: 
+
+let
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
+  session = "${pkgs.niri}/bin/niri-session";
+  username = "aljustiet";
+in
+
+{
+  services.greetd = {
+    enable = true;
+    settings = {
+      initial_session = {
+        command = "${session}";
+        user = "${username}";
+      };
+      default_session = {
+        command = "${tuigreet} --greeting 'Welcome to NixOS!' --asterisks --remember --remember-user-session --time -cmd ${session}";
+        user = "greeter";
+      };
+    };
   };
 
   users.groups.aljustiet = { };
