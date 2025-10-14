f8b2a097cc711cef5b723c0f941da4708154c913
aljustiet <aljustiet@tutamail.com>
2025-10-14 19:48:20 +0500

    update

---
+++ uag.nix
@@ -0,0 +1,23 @@
+{
+  inputs,
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  users.users.root = {
+    shell = pkgs.fish;
+  };
+
+  users.groups.aljustiet = { };
+  users.groups.network = { };
+  users.groups.uinput = { };
+
+  users.users.aljustiet = {
+    isNormalUser = true;
+    group = "aljustiet";
+    uid = 1000;
+    extraGroups = ["wheel" "network" "audio" "video" "input" "uinput"];
+    shell = pkgs.zsh;
+  };
+}
