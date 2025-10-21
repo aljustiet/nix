a64787247cdd887f649d52a1b4e010f1b68e1191
aljustiet <aljustiet@tutamail.com>
2025-10-21 12:04:08 +0500

    update

--- flake.nix
+++ flake.nix
@@ -2,16 +2,24 @@
   description = "NixOS";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    solaar = {
+      url = "https://flakehub.com/f/Svenum/Solaar-Flake/*.tar.gz"; # For latest stable version
+      #url = "https://flakehub.com/f/Svenum/Solaar-Flake/0.1.1.tar.gz"; # uncomment line for solaar version 1.1.13
+      #url = "github:Svenum/Solaar-Flake/main"; # Uncomment line for latest unstable version
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
+    solaar,
     ...
   } @ inputs: {
     nixosConfigurations.thinkpad-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
+        solaar.nixosModules.default
         ./configuration.nix
       ];
       specialArgs = {
