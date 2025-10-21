bec093b9e726cd1b164da15d2502d65790396efe
aljustiet <aljustiet@tutamail.com>
2025-10-21 09:56:06 +0500

    update

--- udev.nix
+++ udev.nix
@@ -4,10 +4,20 @@
   lib,
   inputs,
   ...
-}: {
-  services.udev.extraRules = ''
-    KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"
+}:
+let
+  disableTouchpadWakeUp = true;
+  enableMouseWakeUpRule = false;
+
+  udevRules = [
+    ''KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"''
+  ] ++ (lib.optional disableTouchpadWakeUp ''
     KERNEL=="i2c-SYNA8018:00", SUBSYSTEM=="i2c", ATTR{power/wakeup}="disabled"
+  '') ++ (lib.optional disableTouchpadWakeUp ''
     SUBSYSTEM=="usb", ATTRS{idVendor}=="046d", ATTRS{idProduct}=="c548", ATTR{power/wakeup}="disabled"
-  '';
+  '');
+
+in
+{
+  services.udev.extraRules = lib.concatStringsSep "\n" udevRules;
 }
