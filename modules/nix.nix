ef01a5c30fe253a7e11f74bed9b293e3a50b47a0
aljustiet <aljustiet@tutamail.com>
2025-10-22 09:01:00 +0500

    update

--- modules/nix.nix
+++ modules/nix.nix
@@ -11,6 +11,7 @@
   boot.tmp.tmpfsSize = "50%";
   nix.settings = {
     max-jobs = 16;
+    log-format = "bar-with-logs";
     substituters = [
       "https://cache.nixos.org/"
       "https://nix-community.cachix.org/"
